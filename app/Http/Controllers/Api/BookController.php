c06d633e10a4fa676efd6245fcf04afefdf050ed
huseinsm <huseinbesalen@gmail.com>
2026-03-24 16:51:20 +0700

    initial commit

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Book::all());
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'isbn' => 'required',
+        ]);
+
+        $book = Book::create($request->all());
+
+        return response()->json($book, 201);
+    }
+
+    public function show($id)
+    {
+        return Book::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->update($request->all());
+
+        return response()->json($book);
+    }
+
+    public function destroy($id)
+    {
+        Book::destroy($id);
+
+        return response()->json(['message' => 'Deleted']);
+    }
+}
